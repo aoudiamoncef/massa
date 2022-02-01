b327d63be8a9e2978c1aa2738d0a54aa4dcece60
Your Name <you@example.com>
2022-02-01 14:59:37 +0800

    network: use shared storage

--- massa-graph/src/block_graph.rs
+++ massa-graph/src/block_graph.rs
@@ -1095,6 +1095,9 @@ impl BlockGraph {
                 }
             };
             if cur_a_block.is_final {
+                let block = self.storage.retrieve_block(&cur_a_block.block_id).unwrap();
+                let stored_block = block.read();
+
                 // filters out genesis and final blocks
                 // (step 1.1 in pos.md)
                 final_cycle = cur_a_block.slot.get_cycle(self.cfg.periods_per_cycle);
@@ -1148,7 +1151,6 @@ impl BlockGraph {
                     };
                     // (step 4.1 in pos.md)
                     cur_rolls.apply_updates(&applied_updates)?;
-
                     // (step 4.2 in pos.md)
                     if a_block.slot.get_cycle(self.cfg.periods_per_cycle) == target_cycle {
                         // if the block is in the target cycle, accumulate the roll updates
@@ -1199,6 +1201,9 @@ impl BlockGraph {
                         operations.into_iter().map(|op| Some(op)).collect()
                     };
                     for op in ops.iter() {
+                        let block = self.storage.retrieve_block(&b_id).unwrap();
+                        let stored_block = block.read();
+
                         let (idx, _) = active_block.operation_set.get(op).ok_or_else(|| {
                             GraphError::ContainerInconsistency(format!("op {} should be here", op))
                         })?;
